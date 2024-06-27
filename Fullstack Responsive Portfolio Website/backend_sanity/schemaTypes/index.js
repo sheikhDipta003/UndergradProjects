6980c7ddd1dadcfa2db62952770148b418e81800
sheikhdipta <sheikhdipta@gmail.com>
2024-06-27 19:48:37 +0600

    [REFAC] Fullstack Responsive Portfolio Website added

---
+++ Fullstack Responsive Portfolio Website/backend_sanity/schemaTypes/index.js
@@ -0,0 +1,10 @@
+import testimonials from "../schemas/testimonials";
+import abouts from "../schemas/abouts";
+import brands from "../schemas/brands";
+import contact from "../schemas/contact";
+import experiences from "../schemas/experiences";
+import skills from "../schemas/skills";
+import workExperience from "../schemas/workExperience";
+import works from "../schemas/works";
+
+export const schemaTypes = [testimonials, abouts, brands, contact, experiences, skills, workExperience, works]
