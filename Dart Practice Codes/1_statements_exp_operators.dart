97c564e90b3e969468c6914ae0e6d72fc26efe59
sheikhdipta <sheikhdipta@gmail.com>
2024-06-28 00:06:50 +0600

    [NEW] Dart basics

---
+++ Dart Practice Codes/1_statements_exp_operators.dart
@@ -0,0 +1,24 @@
+
+void main() {
+// Statements
+print('Hello world');
+print('Hello world Two');
+
+// Expressions
+4;
+4 + 1;
+
+// Arithmetic operations
+print(1 + 1);
+print(2 - 1);
+print(3  * 4);
+print(4 / 3);
+// integer division
+print(4 ~/ 3);
+
+
+
+
+  
+
+}
---
+++ Dart Practice Codes/README.md
@@ -0,0 +1,2 @@
+# dart-course 🎯
+This repo containais sample and some concepts definition code from the dart languange course available on [youtube](https://youtu.be/igauQ_rF_bU?si=bbfmq7L8QlqJV0DX)
