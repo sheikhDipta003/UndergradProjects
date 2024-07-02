e01e14041f70301dcdd5d2d96c7848ca85f340c9
sheikhdipta <sheikhdipta@gmail.com>
2024-07-02 19:12:15 +0600

    [FIRST] home,create,profile complete; bookmark remaining

---
+++ React Native Projects/aora/lib/appwrite.js
@@ -0,0 +1,242 @@
+import { Account, Avatars, Client, Databases, ID, Query, Storage } from 'react-native-appwrite';
+
+export const config = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    platform: 'com.siud.aora',
+    projectId: '66802b25001868c532b2',
+    databaseId: '66802c90000fa5843391',
+    userCollectionId: '66802f900037519243a7',
+    videoCollectionId: '66802fc3002438bb702e',
+    storageId: '668036f600175b6fffcf'
+}
+
+// Init your React Native SDK
+const client = new Client();
+
+client
+    .setEndpoint(config.endpoint) // Your Appwrite Endpoint
+    .setProject(config.projectId) // Your project ID
+    .setPlatform(config.platform) // Your application ID or bundle ID.
+;
+
+const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+
+export const createUser = async (email, password, username) => {
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            email,
+            password,
+            username
+        )
+
+        if(!newAccount)  throw Error;
+
+        const avatarUrl = avatars.getInitials(username)
+
+        await signIn(email, password)
+        const newUser = await databases.createDocument(
+            config.databaseId,
+            config.userCollectionId,
+            ID.unique(),
+            {
+                accountId: newAccount.$id,
+                email,
+                username,
+                avatar: avatarUrl
+            }
+        )
+
+        return newUser;
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+}
+
+// Sign In
+export const signIn = async (email, password) => {
+    try {
+        const session = await account.createEmailPasswordSession(email, password);
+
+        return session;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+// Sign Out
+export const signOut = async () => {
+  try {
+    const session = await account.deleteSession('current');
+
+    return session;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export const getCurrentUser = async () => {
+    try {
+        const currentAccount = await account.get();
+
+        if(!currentAccount) throw Error;
+
+        const currentUser = await databases.listDocuments(
+            config.databaseId,
+            config.userCollectionId,
+            [Query.equal('accountId', currentAccount.$id)]
+        )
+
+        if(!currentUser) throw Error;
+
+        return currentUser.documents[0];
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Get video posts created by user
+export const getUserPosts = async (userId) => {
+  try {
+    const posts = await databases.listDocuments(
+      config.databaseId,
+      config.videoCollectionId,
+      [Query.equal("creator", userId), Query.orderDesc("$createdAt")]
+    );
+
+    return posts.documents;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get all video Posts
+export const getAllPosts = async () => {
+    try {
+      const posts = await databases.listDocuments(
+        config.databaseId,
+        config.videoCollectionId,
+        [Query.orderDesc("$createdAt")]
+      );
+  
+      return posts.documents;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+// Get latest created video posts
+export const getLatestPosts = async () => {
+    try {
+      const posts = await databases.listDocuments(
+        config.databaseId,
+        config.videoCollectionId,
+        [Query.orderDesc("$createdAt"), Query.limit(7)]
+      );
+  
+      return posts.documents;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+// Get video posts that matches search query
+export const searchPosts = async (query) => {
+    try {
+      const posts = await databases.listDocuments(
+        config.databaseId,
+        config.videoCollectionId,
+        [Query.search("title", query)]
+      );
+  
+      if (!posts) throw new Error("Something went wrong");
+  
+      return posts.documents;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+// Upload File
+export const uploadFile = async (file, type) => {
+  if (!file) return;
+
+  const asset = {
+    name: file.fileName,
+    type: file.mimeType,
+    size: file.fileSize,
+    uri: file.uri
+  };
+
+  try {
+    const uploadedFile = await storage.createFile(
+      config.storageId,
+      ID.unique(),
+      asset
+    );
+
+    const fileUrl = await getFilePreview(uploadedFile.$id, type);
+    return fileUrl;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Get File Preview
+export const getFilePreview = async (fileId, type) => {
+  let fileUrl;
+
+  try {
+    if (type === "video") {
+      fileUrl = storage.getFileView(config.storageId, fileId);
+    } else if (type === "image") {
+      fileUrl = storage.getFilePreview(
+        config.storageId,
+        fileId,
+        2000,
+        2000,
+        "top",
+        100
+      );
+    } else {
+      throw new Error("Invalid file type");
+    }
+
+    if (!fileUrl) throw Error;
+
+    return fileUrl;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+// Create Video Post
+export const createVideoPost = async (form) => {
+  try {
+    const [thumbnailUrl, videoUrl] = await Promise.all([
+      uploadFile(form.thumbnail, "image"),
+      uploadFile(form.video, "video"),
+    ]);
+
+    const newPost = await databases.createDocument(
+      config.databaseId,
+      config.videoCollectionId,
+      ID.unique(),
+      {
+        title: form.title,
+        thumbnail: thumbnailUrl,
+        video: videoUrl,
+        prompt: form.prompt,
+        creator: form.userId,
+      }
+    );
+
+    return newPost;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
